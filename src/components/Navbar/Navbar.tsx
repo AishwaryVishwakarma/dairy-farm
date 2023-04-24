ca303d1fced48bf8a3ce92bd1a6c539bdbdf5b46
Aishwary Vishwakarma <aishwaryvishwakarma23@gmail.com>
2023-04-24 21:40:11 +0530

    removed navbar theme options

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,33 +5,16 @@ import { UtilityContext } from '../../App'
 const NAV_TABS = ['Home', 'Service', 'Products', 'Contact Us', 'Sign In']
 
 const Navbar: React.FC = () => {
-  const { theme, setTheme, isMobile } = React.useContext(UtilityContext) ?? {}
-
-  function setThemeHandler() {
-    theme === 'light' ? setTheme?.('dark') : setTheme?.('light')
-  }
+  const { isMobile } = React.useContext(UtilityContext) ?? {}
 
   return (
-    <nav
-      className={`${styles.navbar} ${
-        theme === 'light' ? styles.light : styles.dark
-      }`}
-    >
+    <nav className={styles.navbar}>
       <div className={styles.navHeading}>React</div>
       <ul className={styles.navItems}>
         {NAV_TABS.map((tab, idx) => (
           <li key={idx}>{tab}</li>
         ))}
       </ul>
-      {/* <label className={styles.toggleLabel}>
-        <input
-          type="checkbox"
-          onChange={setThemeHandler}
-          checked={theme === 'dark'}
-        />
-        <span className={styles.toggleButton} />
-        <span className={styles.toggleIcons} />
-      </label> */}
     </nav>
   )
 }
