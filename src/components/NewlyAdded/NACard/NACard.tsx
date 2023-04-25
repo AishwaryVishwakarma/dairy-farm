203c638d3255f27687292ecdd7773f59772bf0e3
Dipayan Chakraborty <chakrabortydipayan2012@gmail.com>
2023-04-25 17:19:11 +0530

    made changes to first fold

--- src/components/NewlyAdded/NACard/NACard.tsx
+++ src/components/NewlyAdded/NACard/NACard.tsx
@@ -1,22 +1,31 @@
 import React from 'react'
 import styles from './styles.module.scss'
 import { NewlyAddedProductData as NewlyAddedProductData } from '../../../model'
+import { GrFavorite } from 'react-icons/gr'
+import { AiOutlineShoppingCart } from 'react-icons/ai'
 
 // interface ProductData {
 //   data: NewlyAddedProductData
 // }
 
 const NACard: React.FC<any> = ({ data }) => {
-  const { id, price, title, img } = data ?? {}
+  const { id, price, title, img , item_qty, available_qty} = data ?? {}
 
   return (
     <div className={styles.cardWrapper}>
       <img src={img} alt={title} />
+      <div className={styles.productName}>{title} | {item_qty}</div>
       <div className={styles.priceContainer}>
-        <p>Get it for as low as</p>
-        <span>₹{price}.00</span>
+        MRP ₹{price}
       </div>
-      <div className={styles.productName}>{title}</div>
+      <div className={styles.shopOptions}>
+        {available_qty ? <AiOutlineShoppingCart/> : <p>Out of Stock</p>}
+        
+        
+        <GrFavorite className={styles.icon}/>
+        
+      </div>
+      
     </div>
   )
 }
