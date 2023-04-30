b854a84f1b9f48c0e0daaaedeb14ac3f5c9ae1ca
Aishwary Vishwakarma <aishwaryvishwakarma23@gmail.com>
2023-04-30 18:52:49 +0530

    resolved no-key in testimonials card

--- src/components/commons/TestimonialCard/TestimonialCard.tsx
+++ src/components/commons/TestimonialCard/TestimonialCard.tsx
@@ -1,20 +1,22 @@
 import React from 'react'
 import styles from './styles.module.scss'
 import { AiFillStar } from 'react-icons/ai'
+import { nanoid } from 'nanoid'
 
 const TestimonialCard: React.FC<any> = ({ data }) => {
   const { name, profile_image: image, comment, ratting } = data ?? {}
 
   const STARS = new Array(Number(ratting))
 
-  STARS.fill(<AiFillStar />)
-
-  console.log(data)
   return (
     <div className={styles.testimonialCardWrapper}>
       <img src={image} alt={name} />
       <p className={styles.name}>{name}</p>
-      <div className={styles.stars}>{STARS}</div>
+      <div className={styles.stars}>
+        {STARS.map((_star, idx) => (
+          <AiFillStar key={nanoid()} />
+        ))}
+      </div>
       <div className={styles.reviews}>{ratting} / 5 Reviews</div>
       <p className={styles.comment}>{comment}</p>
     </div>
